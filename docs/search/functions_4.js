05de2aed3067c3d336974751374cdca89c8d8b9a
atrapalis <aristrapalis@gmail.com>
2023-10-29 10:14:50 +0000

    Updated documentation for v2.2.0

--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['ondestroy_0',['OnDestroy',['../classu_lua_1_1_paddle_game_1_1_game.html#a927650e9293408eb0f8dbea2dbd4850e',1,'uLua::PaddleGame::Game']]]
+  ['ondestroy_0',['OnDestroy',['../classu_lua_1_1_paddle_game_1_1_game.html#a927650e9293408eb0f8dbea2dbd4850e',1,'uLua::PaddleGame::Game']]],
+  ['onexpose_1',['OnExpose',['../classu_lua_1_1_paddle_game_1_1_game.html#a2cfd8392fcc22eb70e22dfee7caf3ff8',1,'uLua::PaddleGame::Game']]]
 ];
